3c34541baca35befbeaa7484fb4ae109ed14f746
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2013-04-10 19:47:16 +0200

    Draw the letters that are being carried by the player on the plate

--- Player.js
+++ Player.js
@@ -22,6 +22,10 @@ var Player = GameEntity.extend({
     //letters in the plate
     letters: [],
 
+    //letter placeholders: positions where letters should be drawn
+    //when they are being carried by the player
+    letterPlaceholders: [],
+
     //plate width, it's configurable because it impacts in the difficulty
     plateW: 80,
 
@@ -30,6 +34,24 @@ var Player = GameEntity.extend({
 
         //store plate half width, to speed calculations
         this.plateHW = Math.floor(this.plateW / 2);
+
+        //setup letter placeholders taking into account plate width
+        var letterW = 16;
+        var dx = -1 * this.plateHW;
+        var dy = this.sprite.cy;
+        var x = Math.floor(letterW / 2);
+        var y = -x;
+        while(y > -300) {
+            while(x < this.plateW) {
+                this.letterPlaceholders.push({
+                    x: x + dx,
+                    y: y + dy,
+                });
+                x += letterW;
+            }
+            x = Math.floor(letterW / 2);
+            y -= letterW;
+        }
 
         this._super(ctx);
     },
@@ -66,6 +88,16 @@ var Player = GameEntity.extend({
         //draw plate
         this.ctx.fillRect(this.x - this.plateHW, this.y + this.sprite.cy,
                 this.plateW, 2);
+
+        //draw letters
+        var i = 0;
+        while(i<this.letters.length && i<this.letterPlaceholders.length) {
+            var letter = this.letters[i];
+            letter.x = this.x + this.letterPlaceholders[i].x;
+            letter.y = this.y + this.letterPlaceholders[i].y;
+            letter.draw();
+            i++;
+        }
 
         //draw sprite
         this._super();
