a9fa49439ce560802e17da7eab1e8defe487ac2f
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2013-04-12 18:45:23 +0200

    Licensed code as GPL3. Added full license and headers on each code file

--- Sprite.js
+++ Sprite.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2013 Jacobo Aragunde Pérez <jaragunde@igalia.com>
+ *
+ * This file is part of Alphabet Soup.
+ *
+ * Alphabet Soup is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alphabet Soup is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PhpReport.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * Container of the information related with a sprite
  */
